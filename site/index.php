023bc8917ee0f4c91059f1fec3b780d3f22df328
Henrique da Silva Costa <henriquesc1442@gmail.com>
2023-04-03 11:11:22 -0300

    Create index.php

---
+++ site/index.php
@@ -0,0 +1,29 @@
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projeto Biblioteca</title>
+
+    <link rel="stylesheet" href="estilos/base-site.css">
+</head>
+<body>
+    <?php
+        include('db.php');
+        include('navbar.php')
+    ?>
+
+    <main>
+        <?php
+            if (isset($_GET['pagina'])) {
+                
+            }
+        ?>
+    </main>
+    
+    <?php include('footer.php') ?>
+</body>
+</html>
