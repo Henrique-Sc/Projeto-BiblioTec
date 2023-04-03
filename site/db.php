48ae76e248af8a3147f06ba8db0970e931789c54
Henrique da Silva Costa <henriquesc1442@gmail.com>
2023-04-03 11:11:16 -0300

    Create db.php

---
+++ site/db.php
@@ -0,0 +1,10 @@
+<?php
+   $servidor = "localhost";
+   $usuario = "root";
+   $senha = "";
+   $db = "bdbiblioteca";
+
+   $conexao = mysqli_connect(
+      $servidor, $usuario, $senha, $db
+   );
+?>
