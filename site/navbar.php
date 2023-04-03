affe36ed95b9596cec738e9ced71be87440054c3
Henrique da Silva Costa <henriquesc1442@gmail.com>
2023-04-03 11:11:24 -0300

    Create navbar.php

---
+++ site/navbar.php
@@ -0,0 +1,9 @@
+<nav>
+    <div class="img_logo"><img src="img/biblioteca-logo.png" alt="logo"></div>
+
+    <ul>
+        <li><a href="?alunos">Alunos</a></li>
+        <li><a href="?livros">Livros</a></li>
+        <li><a href="?emprestimos">Empréstimos</a></li>
+    </ul>
+</nav>
