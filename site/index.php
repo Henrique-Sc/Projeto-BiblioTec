d84c9219e0d3f13993822946d46f9fae17e07ffd
Henrique da Silva Costa <henriquesc1442@gmail.com>
2023-04-05 15:42:54 -0300

    Update

--- site/index.php
+++ site/index.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-    <meta charset="UTF-8">
+    <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Projeto Biblioteca</title>
 
     <link rel="stylesheet" href="estilos/base-site.css">
+    <link rel="stylesheet" href="estilos/table.css">
 </head>
 <body>
 
