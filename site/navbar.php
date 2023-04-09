9d438cb94919736d699b3eea28fa5788b0df9f8e
Henrique da Silva Costa <henriquesc1442@gmail.com>
2023-04-09 19:11:09 -0300

    Update navbar.php

--- site/navbar.php
+++ site/navbar.php
@@ -1,9 +1,9 @@
 <nav>
     <div class="img_logo"><a href="?home"><img src="img/biblioteca-logo.png" alt="Logo da bibliotec"></a></div>
 
-    <ul>
-        <li><a href="?alunos">Alunos</a></li>
-        <li><a href="?livros">Livros</a></li>
-        <li><a href="?emprestimos">Empréstimos</a></li>
+    <ul class="nav-itens">
+        <li class="nav-item"><a href="?alunos">Alunos</a></li>
+        <li class="nav-item"><a href="?livros">Livros</a></li>
+        <li class="nav-item"><a href="?emprestimos">Empréstimos</a></li>
     </ul>
 </nav>
