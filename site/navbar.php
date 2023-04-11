1c7d3bc4f1687e864de928e82011434a858a82a8
Etec <etec@etec.com>
2023-04-11 14:40:14 -0300

    Update navbar.php

--- site/navbar.php
+++ site/navbar.php
@@ -1,9 +1,9 @@
 <nav>
-    <div class="img_logo"><a href="?home"><img src="img/biblioteca-logo.png" alt="Logo da bibliotec"></a></div>
+    <div class="img_logo"><a href="?pagina=home"><img src="img/biblioteca-logo.png" alt="Logo da bibliotec"></a></div>
 
     <ul class="nav-itens">
-        <li class="nav-item"><a href="?alunos">Alunos</a></li>
-        <li class="nav-item"><a href="?livros">Livros</a></li>
-        <li class="nav-item"><a href="?emprestimos">Empréstimos</a></li>
+        <li class="nav-item"><a href="?pagina=alunos">Alunos</a></li>
+        <li class="nav-item"><a href="?pagina=livros">Livros</a></li>
+        <li class="nav-item"><a href="?pagina=emprestimos">Empréstimos</a></li>
     </ul>
 </nav>
