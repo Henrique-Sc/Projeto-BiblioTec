479e461c1f4fe31680a46124e9b573738ac90d57
Etec <etec@etec.com>
2023-04-11 15:27:43 -0300

    Create insert_alunos.php

---
+++ site/views/insert_alunos.php
@@ -0,0 +1,18 @@
+<form action="" method="post">
+    <p><input type="text" name="" id="" placeholder="Nome"></p>
+    <p><input type="text" name="" id="" placeholder="Email"></p>
+    <p><input type="text" name="" id="" placeholder="Senha"></p>
+    <p><input type="text" name="" id="" placeholder="Telefone"></p> <!-- (11) 12345-1234 -->
+    <p><input type="date" name="" id=""></p> <!-- dd/mm/aaaa -->
+    
+    <p><input type="text" name="" id="" placeholder="RM"></p>
+    <p><input type="text" name="" id="" placeholder="Curso"></p>
+    <p><input type="text" name="" id="" placeholder="Série"></p>
+    <p><input type="text" name="" id="" placeholder="Ano de ingresso"></p>
+    <p><input type="text" name="" id="" placeholder="RM"></p>
+    <p><input type="submit" value="Enviar"></p>
+</form>
+
+   
+    <th>ANO_INGRESSO</th>  <!-- 9 -->
+    <th>PERIODO</th>       <!-- 10 -->
