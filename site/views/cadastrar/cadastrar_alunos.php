b5b16b0c453ede730279b50182cd2cdf92a248ec
Etec <etec@etec.com>
2023-04-25 16:04:51 -0300

    Atualização do cadastro de alunos

--- site/views/cadastrar/cadastrar_alunos.php
+++ site/views/cadastrar/cadastrar_alunos.php
@@ -1,6 +1,6 @@
 <?php
 //inclusão do arquivo de conexão com o banco de dados
-include 'db.php';
+include('../../database/db.php');
 
 //Recebimento dos valores inseridos no formulário
 //Variável nome vai receber o valor na input nome
@@ -13,13 +13,13 @@ $crso = $_POST['crso'];
 $serie = $_POST['serie'];
 $anoingr = $_POST['anoingr'];
 $perio = $_POST['perio'];
+$email = $_POST['email'];
 
-$query = "INSERT INTO aluno (RM, NOME, TELEFONE, DATA_NASC, EMAIL, SENHA, CURSO, SERIE, ANO_INGRESSO, PERIODO) VALUES 
-(NULL, '$nome', '$psw','$nasc',$tel,$rm,'$curso','$serie','$anoingr','$perio')";
+$query = "INSERT INTO aluno (RM, NOME, TELEFONE, DATA_NASC, EMAIL, SENHA, CURSO, SERIE, ANO_INGRESSO, PERIODO) VALUES ($rm,'$nome',$tel,$nasc,'$email',$psw,'$crso','$serie',$anoingr,'$perio')";
 //executar comando na conexão estabelecida
 mysqli_query($conexao,$query);
 //redirecionamento 
-header('location:index.php?cadastrar_alunos');
+//header('location:index.php?form_alunos');
 
 
 ?>
