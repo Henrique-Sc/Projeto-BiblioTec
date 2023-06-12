240a329f1e33066c8b09e78ed0b95b77434d701c
Henrique da Silva Costa <henriquesc1442@gmail.com>
2023-06-12 14:42:34 -0300

    Update form_alunos.php

--- site/views/form_alunos.php
+++ site/views/form_alunos.php
@@ -42,14 +42,14 @@
         <option value="3">3º ano</option>
     </select>
     
-    <input required type="" name="anoingr" id=""placeholder="Ano de ingresso">
-    
     <select name="perio" id="">
         <option value="Manhã">Manhã</option>
         <option value="Tarde">Tarde</option>
         <option value="Noite">Noite</option>
     </select>
 
+    <input required type="" name="anoingr" id=""placeholder="Ano de ingresso">
+    
     <button class="btnform" type="submit">Enviar</button>
     </div>
 </form>
@@ -59,31 +59,29 @@
         $query = "SELECT * FROM ALUNO WHERE RM = '". $_GET['edt_livro'] . "'";
         $consulta_alunos = mysqli_query($conexao, $query);
         $linha = mysqli_fetch_row($consulta_alunos);
-        var_dump($linha);
+        // var_dump($linha);
     ?>
     
-    <form action="cadastrar/cadastrar_alunos.php" class="formu" method="post">
+    <form action="editar/editar_alunos.php" class="formu" method="post">
         <h2>Cadastro de aluno</h2>
+
+        <input type="hidden" name="rm" value="<?php echo $linha["0"] ?>">
     
         <!-- Nome -->
-        <input required type="text" name="nome" id=""placeholder="Nome" value="<?php $linha[""] ?>">
+        <input required type="text" name="nome" id=""placeholder="Nome" value="<?php echo $linha["1"] ?>">
     
         <!-- Email -->
-        <input required type="text" name="email" id=""placeholder="Email" value="<?php $linha[""] ?>">
+        <input required type="text" name="email" id=""placeholder="Email" value="<?php echo $linha["4"] ?>">
     
         <!-- Senha -->
-        <input required type="text" name="psw" id=""placeholder="Senha" value="<?php $linha[""] ?>">
+        <input required type="text" name="psw" id=""placeholder="Senha" value="<?php echo $linha["5"] ?>">
     
         <!-- Data de nascimento -->
         <label for="nasc">Data de nascimento</label>
-        <input required type="date" name="nasc" id="nasc"placeholder="dd/mm/aaaa" required value="<?php $linha[""] ?>"> <!-- dd/mm/aaaa -->
+        <input required type="date" name="nasc" id="nasc"placeholder="dd/mm/aaaa" required value="<?php echo $linha["3"] ?>"> <!-- dd/mm/aaaa -->
     
         <!-- Telefone -->
-        <input required type="text" name="tel" id=""placeholder="Telefone" value="<?php $linha[""] ?>"> <!-- (11) 12345-1234 -->
-    
-        <!-- RM -->
-        <input required type="text" name="rm" id=""placeholder="RM" value="<?php $linha[""] ?>">
-    
+        <input required type="text" name="tel" id=""placeholder="Telefone" value="<?php echo $linha["2"] ?>"> <!-- (11) 12345-1234 -->    
     
         <!-- Curso -->
         <select name="crso" id="">
@@ -101,13 +99,15 @@
             <option value="3">3º ano</option>
         </select>
         
-        <input required type="" name="anoingr" id=""placeholder="Ano de ingresso" value="<?php $linha[""] ?>">
-        
+        <!-- Período -->
         <select name="perio" id="">
             <option value="Manhã">Manhã</option>
             <option value="Tarde">Tarde</option>
             <option value="Noite">Noite</option>
         </select>
+        
+        <!-- Ano de ingresso -->
+        <input required type="" name="anoingr" id=""placeholder="Ano de ingresso" value="<?php echo $linha["8"] ?>">
     
         <button class="btnform" type="submit">Enviar</button>
         </div>
