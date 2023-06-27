de35401c03eada3fe1276de2b4b500fb6b05ef65
M.C <128746146+MatheusSC404@users.noreply.github.com>
2023-06-27 14:01:11 -0300

    criação do forms login e adição do auxilio para pregs

---
+++ home.php
@@ -0,0 +1,462 @@
+<?php
+
+?>
+
+<h2>Home</h2>
+<!-- Login -->
+<div>
+  <h1 style="text-align: center;"> Bem-vindos a BíblioEtec</h1>
+  <br><br><br>
+  <h3 style="text-align: center;">Preencha os dados para fazer login</h3>
+  <br>
+  <form style="text-align: center;" method="post" action="login.php">
+  <input type="text" name="Usuário" placeholder="Nome do usuário">
+  <br>
+  <input type="password" name="Senha" placeholder="Digite a senha">
+  <br><br>
+  <input type="submit" value="Entrar">
+  <br><br><br><br>
+</div>
+<!-- PRODUTOS -->
+
+<div class="produtos">
+  <div class="product">
+    <div class="imagem">
+      <img src="img/produtos/MonaLisa.jpg" alt="Product Image" />
+    </div>
+    <div class="product-info">
+
+      <h2>Mona Lisa Overdrive</h2>
+
+      <p>Continuação da trilogia Spraw. Clássico de William Gibson</p>
+      <div class="cifrao">
+        <p class="cifra">R$</p>
+        <h2>43,90</h2>
+        <p class="previous">De</p>
+        <h5>54,50</h5>
+      </div>
+
+      <div class="avaliacao">
+        <div class="stars">
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star star-vazia"></i>
+        </div>
+
+        <div class="valor">
+          <p>1372</p>
+        </div>
+      </div>
+
+
+    </div>
+
+  </div>
+
+  <div class="product">
+    <div class="imagem">
+      <img src="img/produtos/OHomemDeGiz.jpg" alt="Product Image" />
+    </div>
+
+    <div class="product-info">
+      <h2>O Homem de Giz</h2>
+
+      <p>Um livro de mistério e suspense sobre um assassinato.</p>
+      <div class="cifrao">
+        <p class="cifra">R$</p>
+        <h2>23,30</h2>
+      </div>
+
+      <div class="avaliacao">
+        <div class="stars">
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star star-vazia"></i>
+          <i class="fa-solid fa-star star-vazia"></i>
+        </div>
+
+        <div class="valor">
+          <p>1129</p>
+        </div>
+      </div>
+
+    </div>
+
+  </div>
+
+
+
+
+  <div class="product">
+    <div class="imagem">
+      <img src="img/produtos/Feed.jpg" alt="Product Image" />
+    </div>
+    <div class="product-info">
+
+      <h2>Quando Ninguém Está Olhando</h2>
+
+      <p>Conheça uma história envolvente</p>
+      <div class="cifrao">
+        <p class="cifra">R$</p>
+        <h2>58,40</h2>
+      </div>
+
+      <div class="avaliacao">
+        <div class="stars">
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+        </div>
+
+        <div class="valor">
+          <p>1457</p>
+        </div>
+      </div>
+
+
+    </div>
+
+  </div>
+
+
+
+
+  <div class="product">
+    <div class="imagem">
+      <img src="img/produtos/TheOutsider.jpg" alt="Product Image" />
+    </div>
+    <div class="product-info">
+
+      <h2>The Outsider</h2>
+
+      <p>Um clássico do terror escrito por Stephen King.</p>
+      <div class="cifrao">
+        <p class="cifra">R$</p>
+        <h2>72,50</h2>
+        <p class="previous">De</p>
+        <h5>104,50</h5>
+      </div>
+
+      <div class="avaliacao">
+        <div class="stars">
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+        </div>
+
+        <div class="valor">
+          <p>17329</p>
+        </div>
+      </div>
+
+
+    </div>
+
+  </div>
+
+
+
+
+
+  <div class="product">
+    <div class="imagem">
+      <img src="img/produtos/Hipotese.jpg" alt="Product Image" />
+    </div>
+    <div class="product-info">
+
+      <h2>A Hipótese do Amor</h2>
+
+      <p>Um romance colegial. Da mesma criadora de Heartstopper</p>
+      <div class="cifrao">
+        <p class="cifra">R$</p>
+        <h2>32,25</h2>
+        <p class="previous">De</p>
+        <h5>45,30</h5>
+      </div>
+
+      <div class="avaliacao">
+        <div class="stars">
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star star-vazia"></i>
+        </div>
+
+        <div class="valor">
+          <p>10329</p>
+        </div>
+      </div>
+
+
+    </div>
+
+  </div>
+
+
+
+
+  <div class="product">
+    <div class="imagem">
+      <img src="img/produtos/opaciente.jpg" alt="Product Image" />
+    </div>
+    <div class="product-info">
+
+      <h2>O Paciente</h2>
+
+      <p>Ele Irá tomar suas noites de sono por diversas noites</p>
+      <div class="cifrao">
+        <p class="cifra">R$</p>
+        <h2>35,50</h2>
+      </div>
+
+      <div class="avaliacao">
+        <div class="stars">
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+        </div>
+
+        <div class="valor">
+          <p>18729</p>
+        </div>
+      </div>
+
+
+    </div>
+
+  </div>
+
+
+
+
+  <div class="product">
+    <div class="imagem">
+      <img src="img/produtos/Manifesto.jpg" alt="Product Image" />
+    </div>
+    <div class="product-info">
+
+      <h2>Manifesto Comunista</h2>
+
+      <p>Livro escrito por Karl Marx e Engels sobre classe trabalhadora</p>
+      <div class="cifrao">
+        <p class="cifra">R$</p>
+        <h2>12,25</h2>
+        <p class="previous">De</p>
+        <h5>25,00</h5>
+      </div>
+
+      <div class="avaliacao">
+        <div class="stars">
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+        </div>
+
+        <div class="valor">
+          <p>147399</p>
+        </div>
+      </div>
+
+
+    </div>
+
+  </div>
+
+
+
+  <div class="product">
+    <div class="imagem">
+      <img src="img/produtos/osdois.jpg" alt="Product Image" />
+    </div>
+    <div class="product-info">
+
+      <h2>Os dois morrem no final</h2>
+
+      <p>Um romance trágico</p>
+      <div class="cifrao">
+        <p class="cifra">R$</p>
+        <h2>32,25</h2>
+        <p class="previous">De</p>
+        <h5>45,30</h5>
+      </div>
+
+      <div class="avaliacao">
+        <div class="stars">
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star star-vazia"></i>
+        </div>
+
+        <div class="valor">
+          <p>10329</p>
+        </div>
+      </div>
+
+
+    </div>
+
+  </div>
+
+
+
+
+  <div class="product">
+    <div class="imagem">
+      <img src="img/produtos/hamlet.jpg" alt="Product Image" />
+    </div>
+    <div class="product-info">
+
+      <h2>Hamlet</h2>
+
+      <p>Um romance colegial. Da mesma criadora de Heartstopper</p>
+      <div class="cifrao">
+        <p class="cifra">R$</p>
+        <h2>32,25</h2>
+        <p class="previous">De</p>
+        <h5>45,30</h5>
+      </div>
+
+      <div class="avaliacao">
+        <div class="stars">
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star star-vazia"></i>
+        </div>
+
+        <div class="valor">
+          <p>10329</p>
+        </div>
+      </div>
+
+
+    </div>
+
+  </div>
+
+
+
+
+  <div class="product">
+    <div class="imagem">
+      <img src="img/produtos/Vilarejo.jpg" alt="Product Image" />
+    </div>
+    <div class="product-info">
+
+      <h2>O Vilarejo</h2>
+
+      <p>Um romance colegial. Da mesma criadora de Heartstopper</p>
+      <div class="cifrao">
+        <p class="cifra">R$</p>
+        <h2>32,25</h2>
+        <p class="previous">De</p>
+        <h5>45,30</h5>
+      </div>
+
+      <div class="avaliacao">
+        <div class="stars">
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star star-vazia"></i>
+        </div>
+
+        <div class="valor">
+          <p>10329</p>
+        </div>
+      </div>
+
+
+    </div>
+
+  </div>
+
+
+  <div class="product">
+    <div class="imagem">
+      <img src="img/produtos/sanatorio.jpg" alt="Product Image" />
+    </div>
+    <div class="product-info">
+
+      <h2>O Sanatório</h2>
+
+      <p>Um romance colegial. Da mesma criadora de Heartstopper</p>
+      <div class="cifrao">
+        <p class="cifra">R$</p>
+        <h2>32,25</h2>
+        <p class="previous">De</p>
+        <h5>45,30</h5>
+      </div>
+
+      <div class="avaliacao">
+        <div class="stars">
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star star-vazia"></i>
+        </div>
+
+        <div class="valor">
+          <p>10329</p>
+        </div>
+      </div>
+
+
+    </div>
+
+  </div>
+
+
+
+  <div class="product">
+    <div class="imagem">
+      <img src="img/produtos/colecionador.jpg" alt="Product Image" />
+    </div>
+
+    <div class="product-info">
+      <h2>O Colecionador</h2>
+
+      <p>Um romance colegial. Da mesma criadora de Heartstopper</p>
+
+      <div class="cifrao">
+        <p class="cifra">R$</p>
+        <h2>32,25</h2>
+        <p class="previous">De</p>
+        <h5>45,30</h5>
+      </div>
+
+      <div class="avaliacao">
+        <div class="stars">
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star star-vazia"></i>
+        </div>
+
+        <div class="valor">
+          <p>10329</p>
+        </div>
+      </div>
+      
+    </div>
+
+  </div>
+</div>
